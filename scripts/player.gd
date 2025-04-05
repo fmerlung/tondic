0182ac82805051b3d219e23b30795b1f6f6c4a1b
Filip Merlung <filip.merlung@gmail.com>
2025-04-05 19:39:08 +0200

    add level 1 prototype

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,21 @@
 extends RigidBody3D
 
+@export var camera: Camera3D;
 const force = 40;
 const max_velocity = 10;
+var velo = 0;
 
-func _physics_process(delta) -> void:
-	if Input.is_action_pressed("ui_up") and self.linear_velocity.z > -max_velocity:
-		self.apply_force(Vector3(0, 0, -force));
-	if Input.is_action_pressed("ui_down") and self.linear_velocity.z < max_velocity:
-		self.apply_force(Vector3(0, 0, force));
-	if Input.is_action_pressed("ui_left") and self.linear_velocity.x > -max_velocity:
-		self.apply_force(Vector3(-force, 0, 0));
-	if Input.is_action_pressed("ui_right") and self.linear_velocity.x < max_velocity:
-		self.apply_force(Vector3(force, 0, 0));
+func _physics_process(delta):
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
+	var camera_forward = (camera.global_transform.basis * Vector3.FORWARD).normalized()
+	var camera_right = (camera.global_transform.basis * Vector3.RIGHT).normalized()
+	camera_forward.y = 0
+	camera_right.y = 0
+	
+	var direction = (camera_right * input_dir.x) + (camera_forward * input_dir.y)
+	apply_central_force(direction.normalized() * force * delta * 20)  # Adjust multiplier
+	
+	velo += delta;
+	if velo >= 0.5: 
+		print(self.linear_velocity.length());
+		velo = 0;
